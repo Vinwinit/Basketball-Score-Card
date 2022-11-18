71d6b1a56d7bb38ed476d581cb460744c3d1c704
Vinwinit <75585885+Vinwinit@users.noreply.github.com>
2022-11-18 20:00:34 +0530

    Source Code Uploaded

---
+++ index.js
@@ -0,0 +1,26 @@
+let count=0;
+function addone(){
+    count=count+1;
+    document.getElementById("display1").textContent=count;
+}
+function addtwo(){
+    count=count+2;
+    document.getElementById("display1").textContent=count;
+}
+function addthree(){
+    count=count+3;
+    document.getElementById("display1").textContent=count;
+}
+let count1=0;
+function adone(){
+    count1=count1+1;
+    document.getElementById("display2").textContent=count1;
+}
+function adtwo(){
+    count1=count1+2;
+    document.getElementById("display2").textContent=count1;
+}
+function adthree(){
+    count1=count1+3;
+    document.getElementById("display2").textContent=count1;
+}
